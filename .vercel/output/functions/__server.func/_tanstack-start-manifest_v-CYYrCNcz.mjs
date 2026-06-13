19fadd0117af7ed55af3b342f0a96fe652ee9b90
git-commit-vishalkumar766 <abhishek2021csdsle002@abesit.edu.in>
2026-06-13 09:13:01 +0530

    chore: ignore and untrack .vercel build artifacts

--- .vercel/output/functions/__server.func/_tanstack-start-manifest_v-CYYrCNcz.mjs
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/ABHISHEK/OneDrive/Desktop/siggytail-main/siggytail-main/src/routes/__root.tsx", children: ["/"], preloads: ["/assets/index-DAO7ZhbM.js"], scripts: [{ attrs: { type: "module", async: true, src: "/assets/index-DAO7ZhbM.js" } }] }, "/": { filePath: "C:/Users/ABHISHEK/OneDrive/Desktop/siggytail-main/siggytail-main/src/routes/index.tsx", children: void 0, preloads: ["/assets/index-DdmXrxdC.js"] } } });
-export {
-  tsrStartManifest
-};
